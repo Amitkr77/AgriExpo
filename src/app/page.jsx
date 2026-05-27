67deb296c4e7019493685b90b66869faf24679a7
Amitkr77 <amitroyk99@gmail.com>
2026-05-27 12:39:25 +0530

    Update favicon and adjust styling in page component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -71,11 +71,11 @@ export default function Page() {
               </div>
 
               {/* Image */}
-              <div className="relative mt-8 overflow-hidden rounded-[2rem]">
+              <div className="relative mt-8 overflow-hidden rounded-4xl">
                 <img
                   src={vegimage.src}
                   alt="Vegetable Powders"
-                  className="h-[450px] w-full object-cover transition-transform duration-700 group-hover:scale-105"
+                  className="h-112.5 w-full object-cover transition-transform duration-700 group-hover:scale-105"
                 />
               </div>
             </Link>
@@ -112,11 +112,11 @@ export default function Page() {
               </div>
 
               {/* Image */}
-              <div className="relative mt-8 overflow-hidden rounded-[2rem]">
+              <div className="relative mt-8 overflow-hidden rounded-4xl">
                 <img
                   src={fruitimage.src}
                   alt="Fruit Powders"
-                  className="h-[450px] w-full object-top transition-transform duration-700 group-hover:scale-105"
+                  className="h-112.5 w-full object-top transition-transform duration-700 group-hover:scale-105"
                 />
               </div>
             </Link>
@@ -129,7 +129,7 @@ export default function Page() {
         <ExportPresence />
         <WhyChooseSriGreen />
         <section className="text-center mb-16">
-          <span className="text-xs tracking-[0.1em] font-bold text-[#7e5700] mb-4 inline bg-black/10 px-5 py-2 rounded-4xl">
+          <span className="text-xs tracking-widest font-bold text-[#7e5700] mb-4 inline bg-black/10 px-5 py-2 rounded-4xl">
             OUR PHILOSOPHY
           </span>
 
@@ -150,7 +150,7 @@ export default function Page() {
               <div className="w-12 h-12 rounded-full border-2 border-white bg-[#ffdcc6]" />
             </div>
 
-            <span className="text-[#414943] text-xs tracking-[0.1em] font-bold">
+            <span className="text-[#414943] text-xs tracking-widest font-bold">
               Trusted by 500+ Global Partners
             </span>
           </div>
